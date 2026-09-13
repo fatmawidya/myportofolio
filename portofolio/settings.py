9084dea4308b0756069321ac1eef58bc8eed8ec3
fatmawidya <fatma.widya@ui.ac.id>
2026-09-14 01:28:54 +0700

    fix: add CSRF trusted origins for PWS

--- portofolio/settings.py
+++ portofolio/settings.py
@@ -19,6 +19,11 @@ load_dotenv()
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "fatma-widya-myportofolio.pws.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.pws.cs.ui.ac.id",
+    "https://fatma-widya-myportofolio.pws.cs.ui.ac.id",
+]
+
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
