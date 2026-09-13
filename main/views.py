ee3010453dfcb5bdc5f10c7a5a456c1dd58af928
fatmawidya <fatma.widya@ui.ac.id>
2026-09-13 18:50:19 +0700

    Update style.css

--- main/views.py
+++ main/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from main.models import Experience
+from main.models import Education, Experience
 
-def show_main(request):
+def show_main(request): #view untuk profil
     context = {
         "name": "Fatma Widya Rachma",
         "npm": "2506533614",
         "study_program": "S1 Sistem Informasi",
         "bio": (
-            "IS @Universitas Indonesia with strong organizational, communication, marketing, and public-facing experience, interested in bridging business and technology."
+            "IS @ Universitas Indonesia, interested in bridging business and technology."
         ),
     }
     return render(request, "index.html", context)
@@ -18,3 +18,10 @@ def show_experience(request):
         "experience_list": Experience.objects.all(),
     }
     return render(request, "experience.html", context)
+
+def show_education(request):
+    context = {
+        "name": "Fatma Widya Rachma", # Nama untuk header/footer
+        "education_list": Education.objects.all(), # Ambil seluruh data dari tabel Education
+        }
+    return render(request, "education.html", context)  # Kirim data ke template education.html baru
